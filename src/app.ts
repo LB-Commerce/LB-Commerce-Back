7422da0209a2937ce6fc8da9ac4a7db3949bba85
unknown <lucaskauandev@gmail.com>
2023-11-14 12:20:49 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata"
+import "express-async-errors"
+import express from "express"
+import { handleAppError } from "./middlewares/handleAppError.middleware"
+
+const app = express()
+app.use(express.json())
+
+
+app.use(handleAppError)
+
+export default app;
